0f78b8e34ab6d5be4c700f6e0aa712bf4b31e7c8
Junhyeong Kim <junhyeong.kim@brainpad.co.jp>
2024-04-01 05:08:00 +0000

    add helper sh

---
+++ scripts/download_dataset/model.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+poetry run huggingface-cli download --repo-type model llm-jp/llm-jp-1.3b-v1.0 --local-dir /home/junhyeong.kim/workspace/MoE-LLaVA-JP/dataset/llm-jp/llm-jp-1.3b-v1.0
---
+++ scripts/download_dataset/pretrain.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+poetry run huggingface-cli download --repo-type dataset liuhaotian/LLaVA-CC3M-Pretrain-595K --local-dir /home/junhyeong.kim/workspace/MoE-LLaVA-JP/dataset/LLaVA-CC3M-Pretrain-595K
